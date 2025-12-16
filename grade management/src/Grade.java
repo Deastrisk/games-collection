ace2fdf0e8317dec72d8115bb56d66f3ef46e79e
Deastrisk <218586909+Deastrisk@users.noreply.github.com>
2025-12-16 20:59:02 +0700

    began grade management
    to learn hashmap, array lists, and very basic classes

---
+++ grade management/src/Grade.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Grade {
+    public static int averageGrade = 0;
+    String[] name;
+    int[] grade;
+    int[] absent;
+
+    public int setAverage() {
+        if ()
+    }
+
+    public static void main(String[] args) throws Exception {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Input your name: ");
+            this.name = scanner.nextLine();
+
+            System.out.println("Input your name: ");
+            this.grade = scanner.nextInt();
+
+            System.out.println("Input your name: ");
+            this.absent = scanner.nextInt();
+        }
+
+        System.out.printf("Your name is : %s", name);
+        System.out.printf("Your name is : %s", name);
+        System.out.printf("Your name is : %s", name);
+    }
+}
