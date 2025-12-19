60f1164c575699a6bf1a2b586c4eaa15cd6d7e15
Deastrisk <218586909+Deastrisk@users.noreply.github.com>
2025-12-19 18:31:48 +0700

    update on simple calculator

--- SimpleCalculator/src/SimpleCalculator.java
+++ SimpleCalculator/src/SimpleCalculator.java
@@ -1,18 +1,23 @@
 
 
 public class SimpleCalculator {
-    double result;
+    double result = 0;
+    String input = "";
     public static final double PI = 3.14159265;
-
-    public SimpleCalculator() {
-        this.result = 5.123;
-    }
 
     public static void main(String[] args) throws Exception {
         SimpleCalculator calculator = new SimpleCalculator();
-        System.out.printf("Output: %f\n", calculator.result);
-        System.out.println("PI: " + SimpleCalculator.PI);
+        calculator.display();
+    }
 
-        // otw
+    private void display() {
+        System.out.println("=== simple calculator ===");
+        System.out.println("Input  : " + input);
+        System.out.println("Result : " + result);
+        System.out.println("[ ( ] [ ) ] [ AC] [DEL]");
+        System.out.println("[ 9 ] [ 8 ] [ 7 ] [ + ]");
+        System.out.println("[ 6 ] [ 5 ] [ 4 ] [ - ]");
+        System.out.println("[ 3 ] [ 2 ] [ 1 ] [ * ]");
+        System.out.println("[ 0 ] [ 0 ] [ = ] [ / ]");
     }
 }
