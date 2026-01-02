c676fed9c260393e707baf7cb18529f3f7f65840
Deastrisk <218586909+Deastrisk@users.noreply.github.com>
2026-01-03 00:41:27 +0700

    downloaded javaFX

---
+++ JavaFXTesting/src/App.java
@@ -0,0 +1,7 @@
+import javafx.application.*;
+
+public class App extends Application {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
