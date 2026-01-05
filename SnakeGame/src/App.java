da69d263fc9cdebbd12bf3980796b6ca2766c30b
Deastrisk <218586909+Deastrisk@users.noreply.github.com>
2026-01-06 00:59:26 +0700

    created window for SnakeGame

--- SnakeGame/src/App.java
+++ SnakeGame/src/App.java
@@ -1,17 +1,7 @@
-import java.awt.Dimension;
-import javax.swing.JFrame;
-import java.util.Map;
-
 public class App {
 
     public static void main(String[] args) throws Exception {
-        JFrame frame = new JFrame();
-        frame.setResizable(true);
-        frame.setTitle("Simple Snake Game");
-        frame.setPreferredSize(new Dimension(700, 400));
-
-        MenuPanel mainMenu = new MenuPanel();
-
-        frame.setVisible(true);
+        Game game = new Game();
+        game.start();
     }
 }
--- SnakeGame/src/Game.java
+++ SnakeGame/src/Game.java
@@ -1,6 +1,8 @@
+import java.awt.*;
+import javax.swing.*;
 import java.util.Map;
 
-public class Game {
+public class Game extends JFrame {
     private final SettingsData settings = new SettingsData();
 
     public Map<PageTypes, Page> pages = Map.ofEntries(
@@ -10,6 +12,34 @@ public class Game {
     );
 
     public Game() {
-        
+        this.setSize(300, 400);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // this.setResizable(false);
+        this.setTitle("Amazing app");
+        ImageIcon icon = new ImageIcon("src\\assets\\snake-logo.png");
+        this.setIconImage(icon.getImage());
+        this.getContentPane().setBackground(Color.darkGray);
+
+        this.setVisible(true);
     }
+
+    public void start() {
+        JLabel title = new JLabel("Snake Game");
+        title.setForeground(Color.white);
+        title.setIcon(new ImageIcon("src\\assets\\snake-logo.png"));
+        title.setVerticalTextPosition(JLabel.TOP);
+        title.setHorizontalTextPosition(JLabel.CENTER);
+        title.setFont(new Font("MV Boli", Font.BOLD, 20));
+        title.setIconTextGap(-5);
+        title.setBackground(Color.blue);
+        title.setOpaque(true);
+
+        title.setVerticalAlignment(JLabel.CENTER);
+        title.setHorizontalAlignment(JLabel.CENTER);
+
+        this.add(title);
+
+    }
+}
+
 }
